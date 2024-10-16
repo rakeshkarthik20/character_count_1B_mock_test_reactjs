8c975d738830307b788b86d21cc83a8ae5b0f050
Rakesh Karthik <rakeshkarthikgrk@gamil.com>
2024-10-16 18:27:10 +0000

    first commit

---
+++ src/components/UserInput/index.js
@@ -0,0 +1,16 @@
+import {UserInputItem, UserEnteredDetails} from './styledComponent'
+
+const UserInput = props => {
+  const {userInputDetails} = props
+  const {userEnteredText, textLength} = userInputDetails
+
+  return (
+    <UserInputItem>
+      <UserEnteredDetails>
+        {userEnteredText} : {textLength}
+      </UserEnteredDetails>
+    </UserInputItem>
+  )
+}
+
+export default UserInput
